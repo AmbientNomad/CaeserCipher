6cdf9eb26782eb35e7ecf05f4163355cbc8b3c3f
AmbientNomad <73661131+AmbientNomad@users.noreply.github.com>
2021-02-02 13:28:00 -0500

    Update ceasercypher.py

--- ceasercypher.py
+++ ceasercypher.py
@@ -3,36 +3,39 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 
 def decoder(user_message, cypher):
-    message_list = [letter for letter in user_message]
-    solved_list = []
+    message_list = [letter for letter in user_message]  # Splits our message to solve letter by letter
+    solved_list = []  # Will store our converted letters before saving as a new message
 
-    for x in message_list:
+    for character in message_list:
 
-        if x.isalpha():
-            change_index = alphabet.index(x.lower()) + cypher
+        if character.isalpha():  # Need to confirm character is a letter, 'else' it returns that character
 
+            # Variable and if statement for determining which letter of the alphabet will be swapped in
+            change_index = alphabet.index(character.lower()) + cypher
             if change_index < 0:
                 change_index = len(alphabet) - abs(change_index)
             elif change_index > len(alphabet) - 1:
                 change_index = change_index - len(alphabet)
 
-            if x.isupper():
+            if character.isupper():  # Simple check to determine upper case or lower case replacement
                 solved_list.append(alphabet[change_index].upper())
             else:
                 solved_list.append(alphabet[change_index])
 
         else:
-            solved_list.append(x)
-            
-    message_solved = "".join(solved_list)
-    return message_solved
+            solved_list.append(character)
+
+    return "".join(solved_list)  # Combines our altered message for printing
 
 
 message = input("What message would you like to alter? ")
-shift_number = int(input("Enter a number, 1-25 (positive or negative), to shift the message "))
+# Allows user to enter any message, encrypted or not. Can be any message, but only A-Z characters will be shifted.
 
-while abs(shift_number) not in range(1, 26):
+shift_number = int(input("Enter a number, 1-25 (positive or negative), to shift the message "))
+# Number required to alter message. 25 max because 26 will result in no change.
+while abs(shift_number) not in range(1, 26):  # Prevent user from entering unusable inputs
     print("That's not an option.")
     shift_number = int(input("Enter a number, 1-25, to shift the message "))
 
+print(message)
 print(decoder(message, shift_number))
